c80d3539b1cc5681c944e502c0e69f8382546305
Serhii Prodan <22973227+serpro69@users.noreply.github.com>
2023-10-18 21:08:15 +0200

    Add extension files

---
+++ script.js
@@ -0,0 +1,352 @@
+const _ = [
+  {
+    "id": "work",
+    "_": [{
+      "url": "https://office.com",
+      "lbl": "office365"
+    },{
+      "url": "https://unit4.cegal.com/app/Index.aspx#",
+      "lbl": "time&expense"
+    },{
+      "url": "https://cloud.cegal.com",
+      "lbl": "intranet"
+    },{
+      "url": "https://jira.elhub.cloud/secure/Dashboard.jspa#Assigned-to-Me/11415",
+      "lbl": "elhub-jira"
+    },{
+      "url": "https://confluence.elhub.cloud/#recently-viewed",
+      "lbl": "elhub-conf"
+    },{
+      "url": "https://phabricator.elhub.cloud/",
+      "lbl": "elhub-phab"
+    },{
+      "url": "http://teamcity.elhub.cloud/overview",
+      "lbl": "elhub-tc"
+    },{
+      "url": "https://jfrog.elhub.cloud/ui/packages",
+      "lbl": "elhub-jfrog"
+    },{
+      "url": "https://code.elhub.cloud",
+      "lbl": "elhub-code"
+    },{
+      "url": "https://build.elhub.cloud",
+      "lbl": "elhub-build"
+    }]
+  },{
+    "id": "personal",
+    "_": [{
+      "url": "https://github.com/orgs/hilarious-ceviche/projects/1/views/13",
+      "lbl": "life-at-edge-of-milkyways"
+    },{
+      "url": "https://trello.com/serpro69/boards",
+      "lbl": "trello"
+    },{
+      "url": "https://github.com",
+      "lbl": "github"
+    },{
+      "url": "https://gitlab.com",
+      "lbl": "gitlab"
+    }]
+  },{
+    "id": "fin",
+    "_": [{
+      "url": "https://www.morningstar.no/no/Market/heatmap.aspx",
+      "lbl": "morningstar-heatmap"
+    }]
+  },{
+    "id": "timesink",
+    "_": [
+      {
+        "url": "https://www.reddit.com/r/Kotlin/",
+        "lbl": "r-kotlin",
+      },
+      {
+        "url": "https://www.reddit.com/r/unixporn",
+        "lbl": "r-unixporn",
+      },
+      {
+        "url": "https://www.reddit.com/r/vim",
+        "lbl": "r-vim",
+      },
+      {
+        "url": "https://www.reddit.com/r/languagelearning/",
+        "lbl": "r-languagelearning",
+      },
+      {
+        "url": "https://stackoverflow.com/",
+        "lbl": "stackoverflow"
+      },
+      {
+        "url": "https://stackexchange.com/",
+        "lbl": "stackexchange"
+      },
+      {
+        "url": "https://pimylifeup.com",
+        "lbl": "pi-tutorials"
+      },
+      {
+        "url": "https://notebooktalk.net/",
+        "lbl": "notebooktalk"
+      },
+      {
+        "url": "https://forums.servethehome.com/index.php",
+        "lbl": "servethehome"
+      },
+      {
+        "url": "https://www.scienceforums.net/",
+        "lbl": "scienceforums"
+      }
+    ]
+  },{
+    "id": "social",
+    "_": [
+      {
+        "url": "https://web.whatsapp.com/",
+        "lbl": "whatsapp",
+      },
+      {
+        "url": "https://youtube.com/",
+        "lbl": "youtube"
+      },
+      {
+        "url": "https://facebook.com",
+        "lbl": "facebook"
+      },
+      {
+        "url": "https://linkedin.com",
+        "lbl": "linkedin"
+      }
+    ]
+  },{
+    "id": "friends",
+    "_": [{
+      "url": "http://z3bra.org",
+      "lbl": "z3bra",
+    },{
+      "url": "https://lyngvaer.no",
+      "lbl": "dcat",
+    },{
+      "url": "http://venam.nixers.net",
+      "lbl": "venam",
+    },{
+      "url": "https://pluvi.us",
+      "lbl": "apk",
+    },{
+      "url": "https://neeasade.net",
+      "lbl": "neeasade",
+    },{
+      "url": "https://mrdoob.com",
+      "lbl": "mr.doob",
+    },{
+      "url": "https://soulwire.co.uk",
+      "lbl": "soulwire",
+    },{
+      "url": "http://zehfernando.com",
+      "lbl": "zeh"
+    }]
+  },{
+    "id": "personal hype",
+    "_": [{
+      "url": "https://xero.nu",
+      "lbl": "xero.nu",
+    },{
+      "url": "https://0w.nz",
+      "lbl": "0w.nz",
+    },{
+      "url": "https://fontvir.us",
+      "lbl": "fontvir.us",
+    },{
+      "url": "https://twitter.com/fontvirus",
+      "lbl": "@fontvirus",
+    },{
+      "url": "https://keybase.io/serpro",
+      "lbl": "keybase"
+    }]
+  },{
+    "id": "learning",
+    "_": [
+      {
+        "url": "https://leetcode.com/",
+        "lbl": "leetcode"
+      },
+      {
+        "url": "https://binarysearch.com/",
+        "lbl": "binarysearch"
+      },
+      {
+        "url": "https://codeforces.com/",
+        "lbl": "codeforces"
+      },
+      {
+        "url": "https://codewars.com/",
+        "lbl": "codewars"
+      }
+    ]
+  },{
+    "id": "reading",
+    "_": [
+      {
+        "url": "https://medium.com",
+        "lbl": "medium"
+      },
+      {
+        "url": "https://blog.kotlin-academy.com/",
+        "lbl": "KotlinAcademy"
+      },
+      {
+        "url": "https://blog.jetbrains.com/kotlin/",
+        "lbl": "JetBrains KotlinBlog"
+      },
+      {
+        "url": "https://kotlinexpertise.com/",
+        "lbl": "KotlinExpertise"
+      },
+      {
+        "url": "https://typealias.com/",
+        "lbl": "TypeAlias (Kotlin)"
+      },
+      {
+        "url": "https://barancev.github.io/",
+        "lbl": "AlexeiBarancev's blog"
+      },
+      {
+        "url": "https://martinfowler.com/",
+        "lbl": "MartinFowler's blog"
+      },
+      {
+        "url": "https://www.developsense.com/",
+        "lbl": "MichaelBolton's blog"
+      },
+      {
+        "url": "https://www.satisfice.com/",
+        "lbl": "JamesBach's blog"
+      },
+      {
+        "url": "http://kaner.com/",
+        "lbl": "CemKaner's blog"
+      },
+      {
+        "url": "https://waitbutwhy.com/#",
+        "lbl": "Tim Urban: Wait But Why"
+      },
+      {
+        "url": "https://www.joelonsoftware.com/",
+        "lbl": "Joel On Software"
+      },
+      {
+        "url": "https://us12.campaign-archive.com/home/?u=f39692e245b94f7fb693b6d82&id=93b2272cb6",
+        "lbl": "kotlin-weekly"
+      },
+      {
+        "url": "",
+        "lbl": ""
+      }
+    ]
+  },{
+    "id": "new",
+    "_": [
+      { "url": "https://forum.mhut.org/", "lbl": "LG Forums" },
+      { "url": "http://libgen.rs/", "lbl": "LG" },
+      { "url": "https://the-eye.eu/", "lbl": "The Eye" },
+      { "url": "https://ravebooksearch.com/", "lbl": "booksearch" },
+      { "url": "", "lbl": "new" },
+      { "url": "", "lbl": "new" }
+    ]
+  },{
+    "id": "entertainment",
+    "_": [
+      { "url": "https://pluto.tv/", "lbl": "Pluto TV" },
+      { "url": "https://youtube.com", "lbl": "YouTube" },
+      { "url": "", "lbl": "new" },
+      { "url": "", "lbl": "new" }
+    ]
+  }
+];
+
+const $ = function $ (selector, context = document) {
+  const elements = Array.from(context.querySelectorAll(selector))
+  return {
+    elements,
+    clear() {
+      this.elements.forEach(element => {
+        element.innerHTML = ""
+      })
+      return this
+    },
+    html(newHtml) {
+      this.elements.forEach(element => {
+        element.innerHTML += newHtml
+      })
+      return this
+    },
+    css(newCss) {
+      this.elements.forEach(element => {
+        Object.assign(element.style, newCss)
+      })
+      return this
+    },
+    focus() {
+      context.querySelector(selector).focus();
+      return this
+    },
+    val() {
+      return context.querySelector(selector).value
+    },
+    on(event, handler, options) {
+      this.elements.forEach(element => {
+        element.addEventListener(event, handler, options)
+      })
+      return this
+    }
+  }
+};
+
+function search() {
+  const find = $("#find").val();
+  const results = [{
+    "id": "filtered",
+    "_": []
+  }];
+  let found = false;
+  if(find.length == 0) {
+    $("#main").clear();
+    build(_);
+    return false;
+  }
+  _.filter((x) => {
+    x._.filter((xx) => {
+      if(xx.lbl.indexOf(find) !=-1 || xx.url.indexOf(find) !=-1) {
+         results[0]._.push({"url": xx.url, "lbl": xx.lbl});
+        found = true;
+      }
+    });
+  });
+  $("#main").clear();
+  build(found ? results : _);
+}
+
+function build(j) {
+  for(const x of j) {
+    let node = `<section><h1>${x.id}</h1><ul>`;
+    for(const l of x._) {
+      if(!l.hidden) {
+        node += `<li><a href="${l.url}">${l.lbl}</a></li>`
+      }
+    }
+    node += `</ul></section>`;
+    $("#main").html(node);
+  }
+}
+
+//(() => {
+  //build(_);
+  //$("#find").on('keypress', search).on('change', search).focus();
+//})();
+
+document.addEventListener("DOMContentLoaded", function () {
+    build(_);
+    const findInput = document.getElementById("find");
+    findInput.addEventListener("keypress", search);
+    findInput.addEventListener("change", search);
+    findInput.focus();
+});

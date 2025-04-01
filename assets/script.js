a81972046bb07ebf2f960b0e8e0908839043112f
serpro69 <22973227+serpro69@users.noreply.github.com>
2025-04-01 10:25:48 +0200

    Add support for devicons

--- assets/script.js
+++ assets/script.js
@@ -1,59 +1,61 @@
 let data;
 
-const $ = function $ (selector, context = document) {
-  const elements = Array.from(context.querySelectorAll(selector))
+const $ = function $(selector, context = document) {
+  const elements = Array.from(context.querySelectorAll(selector));
   return {
     elements,
     clear() {
-      this.elements.forEach(element => {
-        element.innerHTML = ""
-      })
-      return this
+      this.elements.forEach((element) => {
+        element.innerHTML = "";
+      });
+      return this;
     },
     html(newHtml) {
-      this.elements.forEach(element => {
-        element.innerHTML += newHtml
-      })
-      return this
+      this.elements.forEach((element) => {
+        element.innerHTML += newHtml;
+      });
+      return this;
     },
     css(newCss) {
-      this.elements.forEach(element => {
-        Object.assign(element.style, newCss)
-      })
-      return this
+      this.elements.forEach((element) => {
+        Object.assign(element.style, newCss);
+      });
+      return this;
     },
     focus() {
       context.querySelector(selector).focus();
-      return this
+      return this;
     },
     val() {
-      return context.querySelector(selector).value
+      return context.querySelector(selector).value;
     },
     on(event, handler, options) {
-      this.elements.forEach(element => {
-        element.addEventListener(event, handler, options)
-      })
-      return this
-    }
-  }
+      this.elements.forEach((element) => {
+        element.addEventListener(event, handler, options);
+      });
+      return this;
+    },
+  };
 };
 
 function search() {
   const find = $("#find").val();
-  const results = [{
-    "id": "filtered",
-    "_": []
-  }];
+  const results = [
+    {
+      id: "filtered",
+      _: [],
+    },
+  ];
   let found = false;
-  if(find.length == 0) {
+  if (find.length == 0) {
     $("#main").clear();
     build(data);
     return false;
   }
   data.filter((x) => {
     x._.filter((xx) => {
-      if(xx.lbl.indexOf(find) !=-1 || xx.url.indexOf(find) !=-1) {
-         results[0]._.push({"url": xx.url, "lbl": xx.lbl});
+      if (xx.lbl.indexOf(find) != -1 || xx.url.indexOf(find) != -1) {
+        results[0]._.push({ url: xx.url, lbl: xx.lbl });
         found = true;
       }
     });
@@ -63,16 +65,21 @@ function search() {
 }
 
 function build(j) {
-  for(const x of j) {
+  for (const x of j) {
     let node = `<section>
       <h1><i class="fa-solid ${x.icon}"></i> ${x.id}</h1>
       <ul class="fa-ul">`;
-    for(const l of x._) {
-      if(!l.hidden) {
+    for (const l of x._) {
+      if (!l.hidden) {
+        let icon = "";
+        if (l.icon) {
+          icon = `<i class="fas ${l.icon}"></i>`;
+          if (l.icon.startsWith("devicon")) {
+            icon = `<i class="${l.icon}"></i>`
+          }
+        }
         node += `<li>
-          <span class="fa-li">
-            ${l.icon ? `<i class="fas ${l.icon}"></i>` : ``}
-          </span>
+          <span class="fa-li">${icon}</span>
           <a id="url" href="${l.url}">${l.lbl}</a>
         </li>`;
       }
@@ -83,9 +90,9 @@ function build(j) {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  fetch('data.json')
-    .then(response => response.json())
-    .then(jsonData => {
+  fetch("data.json")
+    .then((response) => response.json())
+    .then((jsonData) => {
       data = jsonData;
       build(data);
       const findInput = document.getElementById("find");
@@ -95,7 +102,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
   // add icon on hovering over urls
-/*
+  /*
   document.getElementById("main").addEventListener("mouseover", function(event) {
     if (event.target.matches("a#url")) {
       const iconSpan = event.target.previousElementSibling;
@@ -122,4 +129,3 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 */
 });
-
